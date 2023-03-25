f9de0cbcaa53027c8618b5e8094f9f7a22f79826
Saggitel <timur@Xiaomi-Notebook>
2023-03-25 12:14:02 +0300

    used isort

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets, generics
+from rest_framework import filters, generics, status, viewsets
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
